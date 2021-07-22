412d62a7556b2caabec95d8284994e2ce736c8c3
sharan-sripada <sharan.sripada@zemosolabs.com>
2021-07-22 17:32:57 +0530

    added

---
+++ src/main/java/com/jdbc/GetInstructorCoursesDemo.java
@@ -0,0 +1,56 @@
+package com.jdbc;
+
+import com.jdbc.entity.Course;
+import com.jdbc.entity.Instructor;
+import com.jdbc.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetInstructorCoursesDemo {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            int id=1;
+
+            /*Query<Instructor> query =
+                    session.createQuery("select i from Instructor i "
+                                    + "JOIN FETCH i.courses "
+                                    + "where i.id=:theInstructorId",
+                            Instructor.class);
+
+            query.setParameter("theInstructorId", id);
+
+            Instructor instructor = query.getSingleResult();
+            */
+
+            Instructor instructor=session.get(Instructor.class,id);
+            System.out.println(instructor);
+            //System.out.println(instructor.getCourses());
+
+
+
+            session.getTransaction().commit();
+            session.close();
+            System.out.println("session closed");
+            System.out.println(instructor.getCourses());
+
+            System.out.println("Done");
+        }
+        finally {
+            factory.close();
+        }
+    }
+
+}
